25b855eb4ab7a6500e639ae358724e2a214d5434
ikeersg <segoviaiker10@gmail.com>
2024-04-17 15:31:17 +0200

    segundo comitt

--- bin/pruebas.dart
+++ bin/pruebas.dart
@@ -1,5 +1,4 @@
 import 'package:pruebas/pruebas.dart' as pruebas;
 
-void main(List<String> arguments) {
-  print('Hello world: ${pruebas.calculate()}!');
-}
+void main(List<String> arguments) {}
+//comentrio
