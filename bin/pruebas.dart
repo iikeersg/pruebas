b7165f245e0eb8999e7263dd7ecf19f0cfe93e8f
ikeersg <segoviaiker10@gmail.com>
2024-04-17 15:21:10 +0200

    Primer cambio

---
+++ bin/pruebas.dart
@@ -0,0 +1,5 @@
+import 'package:pruebas/pruebas.dart' as pruebas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pruebas.calculate()}!');
+}
---
+++ test/pruebas_test.dart
@@ -0,0 +1,8 @@
+import 'package:pruebas/pruebas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
